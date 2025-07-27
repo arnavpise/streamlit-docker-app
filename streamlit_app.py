ccb4279af9a5353aa5d3103b99c6fa5b27cd2ad5
Arnav Pise <176802369+arnavpise@users.noreply.github.com>
2025-07-27 21:51:10 +0530

    imported raw data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🚀 My First Streamlit App")
-st.write("LETSSS GOOO... today we'll start making the ml app by refering to the youtube vieo at streamlit")
+st.write("LETSSS GOOO...")
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
